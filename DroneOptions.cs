3ed29629e2f9aaf00533252d73380aa157faf944
WhiteThunder <whetethunger@gmail.com>
2021-06-23 00:22:32 -0700

    Initial commit

---
+++ DroneOptions.cs
@@ -0,0 +1,451 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using Oxide.Core;
+using Rust;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Drone Options", "WhiteThunder", "0.1.0")]
+    [Description("Allows changing toughness, speed and other properties of RC drones.")]
+    internal class DroneOptions : CovalencePlugin
+    {
+        #region Fields
+
+        private static DroneOptions _pluginInstance;
+        private static Configuration _pluginConfig;
+
+        private const string PermissionRulesetPrefix = "droneoptions.ruleset";
+
+        private DroneProperties _vanillaDroneProperties;
+        private ProtectionProperties _vanillaDroneProtection;
+        private List<ProtectionProperties> _customProtectionProperties = new List<ProtectionProperties>();
+
+        #endregion
+
+        #region Hooks
+
+        private void Init()
+        {
+            _pluginInstance = this;
+            _pluginConfig.Init(this);
+
+            Unsubscribe(nameof(OnEntitySpawned));
+        }
+
+        private void OnServerInitialized()
+        {
+            foreach (var entity in BaseNetworkable.serverEntities)
+            {
+                var drone = entity as Drone;
+                if (drone == null)
+                    continue;
+
+                OnEntitySpawned(drone);
+            }
+
+            Subscribe(nameof(OnEntitySpawned));
+        }
+
+        private void Unload()
+        {
+            foreach (var entity in BaseNetworkable.serverEntities)
+            {
+                var drone = entity as Drone;
+                if (drone == null || !IsDroneEligible(drone))
+                    continue;
+
+                if (ApplyRulesetWasBlocked(drone))
+                    continue;
+
+                if (_vanillaDroneProtection != null && _customProtectionProperties.Contains(drone.baseProtection))
+                    drone.baseProtection = _vanillaDroneProtection;
+
+                if (_vanillaDroneProperties != null)
+                    _vanillaDroneProperties?.ApplyToDrone(drone);
+
+                Interface.CallHook("OnDroneOptionsChanged", drone);
+            }
+
+            foreach (var protectionProperties in _customProtectionProperties)
+                UnityEngine.Object.Destroy(protectionProperties);
+
+            _pluginConfig = null;
+            _pluginInstance = null;
+        }
+
+        private void OnEntitySpawned(Drone drone)
+        {
+            if (!IsDroneEligible(drone))
+                return;
+
+            if (_vanillaDroneProtection == null && drone.baseProtection != null)
+                _vanillaDroneProtection = drone.baseProtection;
+
+            if (_vanillaDroneProperties == null)
+                _vanillaDroneProperties = DroneProperties.FromDrone(drone);
+
+            // Delay to give other plugins a moment to cache the drone id so they can block this.
+            NextTick(() =>
+            {
+                var ruleset = GetDroneRuleset(drone.OwnerID);
+                if (ruleset == null)
+                    return;
+
+                TryApplyRuleset(drone, ruleset);
+            });
+        }
+
+        #endregion
+
+        #region API
+
+        private void ResetDroneOptions(Drone drone)
+        {
+            var ruleset = GetDroneRuleset(drone.OwnerID);
+            if (ruleset == null)
+                return;
+
+            TryApplyRuleset(drone, ruleset);
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private static bool ApplyRulesetWasBlocked(Drone drone)
+        {
+            object hookResult = Interface.CallHook("OnDroneOptionsChange", drone);
+            return hookResult is bool && (bool)hookResult == false;
+        }
+
+        private static float Clamp(float x, float min, float max) => Math.Max(min, Math.Min(x, max));
+
+        private static bool IsDroneEligible(Drone drone) => !(drone is DeliveryDrone);
+
+        private static string GetRulesetPermission(string name) =>
+            $"{PermissionRulesetPrefix}.{name}";
+
+        private static bool TryApplyRuleset(Drone drone, DroneRuleset ruleset)
+        {
+            if (ApplyRulesetWasBlocked(drone))
+                return false;
+
+            ruleset.ApplyToDrone(drone);
+            Interface.CallHook("OnDroneOptionsChanged", drone);
+            return true;
+        }
+
+        private ProtectionProperties CreateProtectionProperties(Dictionary<string, float> damageMap)
+        {
+            var protectionProperties = ScriptableObject.CreateInstance<ProtectionProperties>();
+            _customProtectionProperties.Add(protectionProperties);
+
+            foreach (var entry in damageMap)
+            {
+                DamageType damageType;
+                if (!Enum.TryParse<DamageType>(entry.Key, true, out damageType))
+                {
+                    _pluginInstance.LogError($"Invalid damage type: {entry.Key}");
+                    continue;
+                }
+                protectionProperties.Add(damageType, 1 - Clamp(entry.Value, 0, 1));
+            }
+
+            return protectionProperties;
+        }
+
+        #endregion
+
+        #region Configuration
+
+        private DroneRuleset GetDroneRuleset(ulong ownerId)
+        {
+            if (ownerId == 0 || (_pluginConfig.Rulesets?.Length ?? 0) == 0)
+                return _pluginConfig.DefaultRuleset;
+
+            var ownerIdString = ownerId.ToString();
+            for (var i = _pluginConfig.Rulesets.Length - 1; i >= 0; i--)
+            {
+                var ruleset = _pluginConfig.Rulesets[i];
+                if (ruleset.Permission != null && permission.UserHasPermission(ownerIdString, ruleset.Permission))
+                    return ruleset;
+            }
+
+            return _pluginConfig.DefaultRuleset;
+        }
+
+        private class Configuration : SerializableConfiguration
+        {
+            [JsonProperty("DefaultRuleset")]
+            public DroneRuleset DefaultRuleset = new DroneRuleset()
+            {
+                DamageScale = new Dictionary<string, float>(),
+                DroneProperties = new DroneProperties(),
+            };
+
+            [JsonProperty("Rulesets")]
+            public DroneRuleset[] Rulesets = new DroneRuleset[]
+            {
+                new DroneRuleset()
+                {
+                    Name = "balanced",
+                    DamageScale = new Dictionary<string, float>()
+                    {
+                        [DamageType.Generic.ToString()] = 0.1f,
+                        [DamageType.Heat.ToString()] = 0.2f,
+                        [DamageType.Bullet.ToString()] = 0.2f,
+                        [DamageType.Radiation.ToString()] = 0,
+                        [DamageType.AntiVehicle.ToString()] = 0.25f,
+                        [DamageType.Collision.ToString()] = 0,
+                    },
+                },
+                new DroneRuleset()
+                {
+                    Name = "god",
+                    DroneProperties = new DroneProperties()
+                    {
+                        KillInWater = false,
+                        MovementAcceleration = 30,
+                        AltitudeAcceleration = 20,
+                        LeanWeight = 0,
+                    },
+                    DamageScale = new Dictionary<string, float>()
+                    {
+                        [DamageType.AntiVehicle.ToString()] = 0,
+                        [DamageType.Arrow.ToString()] = 0,
+                        [DamageType.Bite.ToString()] = 0,
+                        [DamageType.Bleeding.ToString()] = 0,
+                        [DamageType.Blunt.ToString()] = 0,
+                        [DamageType.Bullet.ToString()] = 0,
+                        [DamageType.Cold.ToString()] = 0,
+                        [DamageType.ColdExposure.ToString()] = 0,
+                        [DamageType.Collision.ToString()] = 0,
+                        [DamageType.Decay.ToString()] = 0,
+                        [DamageType.Drowned.ToString()] = 0,
+                        [DamageType.ElectricShock.ToString()] = 0,
+                        [DamageType.Explosion.ToString()] = 0,
+                        [DamageType.Fall.ToString()] = 0,
+                        [DamageType.Fun_Water.ToString()] = 0,
+                        [DamageType.Generic.ToString()] = 0,
+                        [DamageType.Heat.ToString()] = 0,
+                        [DamageType.Hunger.ToString()] = 0,
+                        [DamageType.Poison.ToString()] = 0,
+                        [DamageType.Radiation.ToString()] = 0,
+                        [DamageType.RadiationExposure.ToString()] = 0,
+                        [DamageType.Slash.ToString()] = 0,
+                        [DamageType.Stab.ToString()] = 0,
+                        [DamageType.Suicide.ToString()] = 0,
+                        [DamageType.Thirst.ToString()] = 0,
+                    },
+                },
+            };
+
+            public void Init(DroneOptions pluginInstance)
+            {
+                DefaultRuleset.Init(pluginInstance, requiresPermission: false);
+
+                foreach (var ruleset in Rulesets)
+                    ruleset.Init(pluginInstance, requiresPermission: true);
+            }
+        }
+
+        private class DroneRuleset
+        {
+            [JsonProperty("Name", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public string Name;
+
+            [JsonProperty("DroneProperties")]
+            public DroneProperties DroneProperties = new DroneProperties();
+
+            [JsonProperty("DamageScale")]
+            public Dictionary<string, float> DamageScale = new Dictionary<string, float>();
+
+            [JsonIgnore]
+            public ProtectionProperties ProtectionProperties;
+
+            [JsonIgnore]
+            public string Permission;
+
+            public void Init(DroneOptions pluginInstance, bool requiresPermission)
+            {
+                if (requiresPermission)
+                {
+                    if (string.IsNullOrWhiteSpace(Name))
+                        return;
+
+                    Permission = GetRulesetPermission(Name);
+                    pluginInstance.permission.RegisterPermission(Permission, pluginInstance);
+                }
+
+                if (DamageScale != null)
+                    ProtectionProperties = pluginInstance.CreateProtectionProperties(DamageScale);
+            }
+
+            public void ApplyToDrone(Drone drone)
+            {
+                if (ProtectionProperties != null)
+                    drone.baseProtection = ProtectionProperties;
+
+                if (DroneProperties != null)
+                    DroneProperties.ApplyToDrone(drone);
+            }
+        }
+
+        private class DroneProperties
+        {
+            public static DroneProperties FromDrone(Drone drone)
+            {
+                var properties = new DroneProperties()
+                {
+                    KillInWater = drone.killInWater,
+                    MovementAcceleration = drone.movementAcceleration,
+                    AltitudeAcceleration = drone.altitudeAcceleration,
+                    LeanWeight = drone.leanWeight,
+                };
+
+                return properties;
+            }
+
+            [JsonProperty("KillInWater", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public bool? KillInWater;
+
+            [JsonProperty("MovementAcceleration", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public float? MovementAcceleration;
+
+            [JsonProperty("AltitudeAcceleration", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public float? AltitudeAcceleration;
+
+            [JsonProperty("LeanWeight", DefaultValueHandling = DefaultValueHandling.Ignore)]
+            public float? LeanWeight;
+
+            public void ApplyToDrone(Drone drone)
+            {
+                if (KillInWater != null)
+                    drone.killInWater = (bool)KillInWater;
+
+                if (MovementAcceleration != null)
+                    drone.movementAcceleration = (float)MovementAcceleration;
+
+                if (AltitudeAcceleration != null)
+                    drone.altitudeAcceleration = (float)AltitudeAcceleration;
+
+                if (LeanWeight != null)
+                    drone.leanWeight = (float)LeanWeight;
+            }
+        }
+
+        private Configuration GetDefaultConfig() => new Configuration();
+
+        #endregion
+
+        #region Configuration Boilerplate
+
+        private class SerializableConfiguration
+        {
+            public string ToJson() => JsonConvert.SerializeObject(this);
+
+            public Dictionary<string, object> ToDictionary() => JsonHelper.Deserialize(ToJson()) as Dictionary<string, object>;
+        }
+
+        private static class JsonHelper
+        {
+            public static object Deserialize(string json) => ToObject(JToken.Parse(json));
+
+            private static object ToObject(JToken token)
+            {
+                switch (token.Type)
+                {
+                    case JTokenType.Object:
+                        return token.Children<JProperty>()
+                                    .ToDictionary(prop => prop.Name,
+                                                  prop => ToObject(prop.Value));
+
+                    case JTokenType.Array:
+                        return token.Select(ToObject).ToList();
+
+                    default:
+                        return ((JValue)token).Value;
+                }
+            }
+        }
+
+        private bool MaybeUpdateConfig(SerializableConfiguration config)
+        {
+            var currentWithDefaults = config.ToDictionary();
+            var currentRaw = Config.ToDictionary(x => x.Key, x => x.Value);
+            return MaybeUpdateConfigDict(currentWithDefaults, currentRaw);
+        }
+
+        private bool MaybeUpdateConfigDict(Dictionary<string, object> currentWithDefaults, Dictionary<string, object> currentRaw)
+        {
+            bool changed = false;
+
+            foreach (var key in currentWithDefaults.Keys)
+            {
+                object currentRawValue;
+                if (currentRaw.TryGetValue(key, out currentRawValue))
+                {
+                    var defaultDictValue = currentWithDefaults[key] as Dictionary<string, object>;
+                    var currentDictValue = currentRawValue as Dictionary<string, object>;
+
+                    if (defaultDictValue != null)
+                    {
+                        if (currentDictValue == null)
+                        {
+                            currentRaw[key] = currentWithDefaults[key];
+                            changed = true;
+                        }
+                        else if (MaybeUpdateConfigDict(defaultDictValue, currentDictValue))
+                            changed = true;
+                    }
+                }
+                else
+                {
+                    currentRaw[key] = currentWithDefaults[key];
+                    changed = true;
+                }
+            }
+
+            return changed;
+        }
+
+        protected override void LoadDefaultConfig() => _pluginConfig = GetDefaultConfig();
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                _pluginConfig = Config.ReadObject<Configuration>();
+                if (_pluginConfig == null)
+                {
+                    throw new JsonException();
+                }
+
+                if (MaybeUpdateConfig(_pluginConfig))
+                {
+                    LogWarning("Configuration appears to be outdated; updating and saving");
+                    SaveConfig();
+                }
+            }
+            catch
+            {
+                LogWarning($"Configuration file {Name}.json is invalid; using defaults");
+                LoadDefaultConfig();
+            }
+        }
+
+        protected override void SaveConfig()
+        {
+            Log($"Configuration changes saved to {Name}.json");
+            Config.WriteObject(_pluginConfig, true);
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,136 @@
+## Features
+
+- Allows changing toughness, speed and other properties of RC drones
+- Allows multiple rulesets based on permissions
+
+## Permissions
+
+The following permissions come with this plugin's **default configuration**. Granting one to a player determine the properties of drones they deploy.
+
+- `droneoptions.ruleset.balanced` -- Vanilla speed, balanced toughness.
+- `droneoptions.ruleset.god` -- Invincible drones with super speed, no altitude loss while moving, and able to go underwater.
+
+You can add more drone rulesets in the plugin configuration, and the plugin will automatically generate permissions of the format `droneoptions.ruleset.<name>` when reloaded. If a player has permission to multiple rulesets, only the last one will apply, based on the order in the config.
+
+## Configuration
+
+Default configuration:
+
+```json
+{
+  "DefaultRuleset": {
+    "DroneProperties": {},
+    "DamageScale": {}
+  },
+  "Rulesets": [
+    {
+      "Name": "balanced",
+      "DroneProperties": {},
+      "DamageScale": {
+        "Generic": 0.1,
+        "Heat": 0.2,
+        "Bullet": 0.2,
+        "Radiation": 0.0,
+        "AntiVehicle": 0.25,
+        "Collision": 0.0
+      }
+    },
+    {
+      "Name": "god",
+      "DroneProperties": {
+        "KillInWater": false,
+        "MovementAcceleration": 30.0,
+        "AltitudeAcceleration": 30.0,
+        "LeanWeight": 0.0
+      },
+      "DamageScale": {
+        "AntiVehicle": 0.0,
+        "Arrow": 0.0,
+        "Bite": 0.0,
+        "Bleeding": 0.0,
+        "Blunt": 0.0,
+        "Bullet": 0.0,
+        "Cold": 0.0,
+        "ColdExposure": 0.0,
+        "Collision": 0.0,
+        "Decay": 0.0,
+        "Drowned": 0.0,
+        "ElectricShock": 0.0,
+        "Explosion": 0.0,
+        "Fall": 0.0,
+        "Fun_Water": 0.0,
+        "Generic": 0.0,
+        "Heat": 0.0,
+        "Hunger": 0.0,
+        "Poison": 0.0,
+        "Radiation": 0.0,
+        "RadiationExposure": 0.0,
+        "Slash": 0.0,
+        "Stab": 0.0,
+        "Suicide": 0.0,
+        "Thirst": 0.0
+      }
+    }
+  ]
+}
+```
+
+- `DefaultRuleset` -- The default ruleset applies to all drones, except for drones deployed by players with permission to another ruleset.
+  - `DamageScale` (each `0.0` to `1.0`) -- These options determine how much damage the drone will take, per damage type.
+    - Set a damage type to `1.0` (default) to take full damage.
+    - Set a damage type to `0.0` to block all damage of that type.
+    - Note: For some reason, Drone collision uses `Generic` damage, not `Collision` damage.
+  - `DroneProperties`
+    - `KillInWater` (vanilla: `true`) -- While `true`, the drone will be destroyed when it enters water.
+      - Tip: While controlling a drone that is underwater, you can actually see better if wearing a diving mask.
+    - `MovementAcceleration` (vanilla: `10.0`) -- This determines the drone's horizontal movement speed.
+    - `AltitudeAcceleration` (vanilla: `10.0`) -- This determines the drone's vertical movement speed (how quickly it can go up and down).
+    - `LeanWeight` (vanilla: `0.25`) -- This determines how much the drone leans while moving, as well as how much altitude is lost while moving.
+      - Set to `0.0` for no lean or altitude loss.
+        - Useful when using the [Drone Lights](https://umod.org/plugins/drone-lights) plugin since it prevents the beam from unintentionally moving.
+        - Useful or when flying in locations where the altitude does not change, such as in the underground train tunnels.
+- `Rulesets` -- List of rulesets that require permission. Each ruleset generates a permssion of the format `droneoptions.ruleset.<name>`. Granting one to a player determines the properties of drones they deploy, overriding `DefaultRuleset`.
+  - Each ruleset has the same options as `DefaultRuleset`.
+
+## FAQ
+
+#### How do I get a drone?
+
+As of this writing, RC drones are a deployable item named `drone`, but they do not appear naturally in any loot table, nor are they craftable. However, since they are simply an item, you can use plugins to add them to loot tables, kits, GUI shops, etc. Admins can also get them with the command `inventory.give drone 1`, or spawn one in directly with `spawn drone.deployed`.
+
+#### How do I remote-control a drone?
+
+If a player has building privilege, they can pull out a hammer and set the ID of the drone. They can then enter that ID at a computer station and select it to start controlling the drone. Controls are `W`/`A`/`S`/`D` to move, `shift` (sprint) to go up, `ctrl` (duck) to go down, and mouse to steer.
+
+Note: If you are unable to steer the drone, that is likely because you have a plugin drawing a UI that is grabbing the mouse cursor. The Movable CCTV was previously guilty of this and was patched in March 2021.
+
+## Recommended compatible plugins
+
+- [Drone Hover](https://umod.org/plugins/drone-hover) -- Allows RC drones to hover in place while not being controlled.
+- [Drone Lights](https://umod.org/plugins/drone-lights) -- Adds controllable search lights to RC drones.
+- [Drone Storage](https://umod.org/plugins/drone-storage) -- Allows players to deploy a small stash to RC drones.
+- [Drone Turrets](https://umod.org/plugins/drone-turrets) -- Allows players to deploy auto turrets to RC drones.
+- [Drone Effects](https://umod.org/plugins/drone-effects) -- Adds collision effects and propeller animations to RC drones.
+- [Auto Flip Drones](https://umod.org/plugins/auto-flip-drones) -- Auto flips upside-down RC drones when a player takes control.
+- [RC Identifier Fix](https://umod.org/plugins/rc-identifier-fix) -- Auto updates RC identifiers saved in computer stations to refer to the correct entity.
+
+## Developer Hooks
+
+#### OnDroneOptionsChange
+
+```csharp
+bool? OnDroneOptionsChange(Drone drone)
+```
+
+- Called when this plugin is about to alter a drone
+- Returning `false` will prevent the the drone from being altered
+- Returning `null` will result in the default behavior
+
+#### OnDroneOptionsChanged
+
+```csharp
+void OnDroneOptionsChanged(Drone drone, SearchLight searchLight)
+```
+
+- Called after this plugin has altered a drone
+- No return behavior
